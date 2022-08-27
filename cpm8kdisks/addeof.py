e7d83cd8129ee74523ad28beb177b109bd34e68c
Scott Baker <smbaker@gmail.com>
2022-08-27 11:34:19 -0700

    change diskdefs for better fit. better addeof.py

--- cpm8kdisks/addeof.py
+++ cpm8kdisks/addeof.py
@@ -1,13 +1,34 @@
 from __future__ import print_function
 import sys
+import os
 
 def addlf(fn):
   print(fn)
-  f = open(fn, "r+")
-  data = f.read()
-  if data[-1] != chr(0x1A):
-    f.write(chr(0x1A))
+  f = open(fn, "rb")
+  data_orig = f.read()
 
+  data = ""
+
+  # also add the CRs here...
+  for ch in data_orig:
+    if (ch==chr(0x0A)):
+      data += chr(0x0D)
+    data += ch
+
+  while data[-1] == chr(0x1A):
+    data = data[:-1]
+
+  data = data + chr(0x1A)
+
+  if fn.endswith(".sub"):
+    while (len(data)%128) != 0:
+      data = data + chr(0x1A)
+
+  if data != data_orig:
+    f = open(fn+".new", "wb")
+    f.write(data)
+    f.close()
+    os.system("mv %s.new %s" % (fn, fn))
 
 for fn in sys.argv[1:]:
   addlf(fn)
